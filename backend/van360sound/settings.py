d65730c7446c3e119c274f1cecf0388436e44559
van360sound <ruvedia@hotmail.com>
2026-01-18 20:47:08 +0100

    Change static files storage to be more lenient on missing files

--- backend/van360sound/settings.py
+++ backend/van360sound/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static and Media files
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
