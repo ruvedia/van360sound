864fdaf15cc373180544064332a6497671e2016b
van360sound <ruvedia@hotmail.com>
2026-01-19 23:03:29 +0100

    fix(media): Servir archivos media en producción
    - Habilitar vista 'serve' de Django para /media/ cuando DEBUG=False
    - Soluciona error 404 en imágenes subidas en Render

--- backend/van360sound/urls.py
+++ backend/van360sound/urls.py
@@ -24,5 +24,12 @@ urlpatterns = [
     path('api/', include('blog.urls')),
 ]
 
+from django.views.static import serve
+from django.urls import re_path
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
