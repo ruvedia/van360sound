90684409476184e7e6bf16315632d73265188988
van360sound <ruvedia@hotmail.com>
2026-01-26 17:44:31 +0100

    fix: Robust build script with converting pip and explicit psycopg2-binary install

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
+pip install psycopg2-binary
 
 python manage.py collectstatic --no-input
 python manage.py migrate
