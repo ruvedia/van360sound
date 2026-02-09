e211b88a7837ff78b01cb5b86dd554c1c86121fd
van360sound <ruvedia@hotmail.com>
2026-02-09 19:09:27 +0100

    fix: Enable page_size query param to allow fetching all headphones

--- backend/van360sound/settings.py
+++ backend/van360sound/settings.py
@@ -185,6 +185,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 12,
+    'PAGE_SIZE_QUERY_PARAM': 'page_size',
 }
 
 # Email Configuration
