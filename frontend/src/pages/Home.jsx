69d886e15c3b0dd75c31cf7fd8e99ce18a852dd6
van360sound <ruvedia@hotmail.com>
2026-03-04 07:55:10 +0100

    fix(ui): add missing Link import in Home.jsx causing blank screen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { headphoneService, articleService } from '../services/api';
 import HeadphoneCard from '../components/HeadphoneCard';
 import ArticleCard from '../components/ArticleCard';
